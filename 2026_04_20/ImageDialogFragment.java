128cd518336c744617d16f308b232865e0119032
PiwońskiS <144985794+piwonskis@users.noreply.github.com>
2026-04-20 13:42:17 +0200

    Add files via upload

---
+++ 2026_04_20/ImageDialogFragment.java
@@ -0,0 +1,35 @@
+package zsk.pack.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ImageDialogFragment extends DialogFragment {
+
+    private static final String ARG_IMAGE = "arg_image";
+
+    public static ImageDialogFragment newInstance(int imageResId) {
+        ImageDialogFragment fragment = new ImageDialogFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_IMAGE, imageResId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        int imageResId = getArguments().getInt(ARG_IMAGE);
+
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(imageResId);
+        imageView.setAdjustViewBounds(true);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(imageView);
+
+        return builder.create();
+    }
+}
---
+++ 2026_04_20/MainActivity.java
@@ -0,0 +1,39 @@
+package zsk.pack.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    public void showImage(View v) {
+        int imageId = (int) v.getTag();
+        ImageDialogFragment dialog = ImageDialogFragment.newInstance(imageId);
+        dialog.show(getSupportFragmentManager(), "image_dialog");
+    }
+}
